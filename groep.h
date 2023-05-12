5437aa5ebf20eaf4344dee33d0ea7c050d260e2b
Gnommieualdo <romualdo98@tutanota.com>
2023-05-13 01:31:04 +0200

    comments in groep.h

--- groep.h
+++ groep.h
@@ -16,25 +16,61 @@ class Groep
 
     // Default constructor
     Groep ();
+    /*
+     * constructor voor het instantieren van grenswaarden van de groep.
+     *
+     */
     Groep (const int maxSom, const int maxVoorkomens);
-    void insert(pair<int,int> coordinaten, int waarde);
-    void erase(pair<int,int> coordinaten);
-    static void setDomain(unordered_set<int> domein);
-    static my_set getDomain();
-    static bool commit;
-    void commitInsert(pair<int,int> coordinaten, int waarde);
-    void commitErase(pair<int,int> coordinaten);
-    my_set getResterendeKeuzes();
 
-    // TODO: uw eigen publieke memberfuncties
+    /*
+     * functie voor een insert verzoek, indien de check van deze en de andere
+     * groepen volstaan,
+     * kan men waarde op coordinaten invullen
+     */
+    void insert(pair<int,int> coordinaten, int waarde);
+
+    /*
+     * functie voor een erase verzoek, indien de check van deze en de andere
+     * groepen volstaan,
+     * kan men de waarde op deze coordinaat verwijderen. Als de waarde niet
+     * ingevuld is, zal er niets gebeuren.
+     */
+    void erase(pair<int,int> coordinaten);
+    /*
+     * static setter voor keuzes.
+     */
+    static void setDomain(unordered_set<int> domein);
+    /*
+     * static getter voor keuzes.
+     */
+    static my_set getDomain();
+
+    /*
+     * Voert inserts uit
+     * post:
+     * - als commit == true, dan zal in elke groep van deze coordinaat
+     * de waarde ingevuld zijn.
+     */
+    void commitInsert(pair<int,int> coordinaten, int waarde);
+    /*
+     * voert Erase uit
+     * post:
+     * - als commit == true, dan zal in elke groep van deze coordinaat
+     * de waarde ingevuld zijn
+     */
+    void commitErase(pair<int,int> coordinaten);
+    /*
+     * getter voor resterendeKeuzes.
+     */
+    my_set getResterendeKeuzes();
+    static bool commit; // Geeft aan of gecheckte groepen door moeten gaan met insert/delete.
 
   private:
-    map<int, int> invulbareWaardes;
-    map<pair<int,int>,int> ingevuldeVakjes;
-    int somResterend;
-    static my_set keuzes;
-    my_set resterendeKeuzes;
-    // TODO: uw eigen private memberfuncties en -variabelen
+    map<int, int> invulbareWaardes;         // invulbareWaardes samen met de hoeveelheid die nog mogen ingevoerd worden.
+    map<pair<int,int>,int> ingevuldeVakjes;  // vakjes die ingevuld zijn, samen met de ingevulde waarde.
+    int somResterend;               // wat er nog resteert van maxSom.
+    static my_set keuzes;          // domein van waardes die ingevuld mogen worden.
+    my_set resterendeKeuzes;       // waardes die nog ingevoerd mogen worden, in set vorm.
 
 };
 
